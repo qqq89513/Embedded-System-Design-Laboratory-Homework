044ce9bb76c8f5014723d9feac86d3f9d3c1691c
Quence Z <qqq89513@yahoo.com.tw>
2020-11-10 15:41:25 +0800

    Initial commit of hw01: GUI calculator with touchGFX
    touchGFX project file: ./hw01_calc/TouchGFX
    stm32cubeIDE project file : ./hw01_calc/STM32CubeIDE/.project
    
    main.c: ./hw01_calc/Core/Src/main.c
    GUI widget callbacks:
    　　./hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
    　　./hw01_calc/TouchGFX/gui/inc/screen_screen/screenView.hpp

---
+++ hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
@@ -0,0 +1,18 @@
+#include <gui/screen_screen/screenView.hpp>
+#include <main.h>
+
+screenView::screenView()
+{
+
+}
+
+void screenView::setupScreen()
+{
+    screenViewBase::setupScreen();
+}
+
+void screenView::tearDownScreen()
+{
+    screenViewBase::tearDownScreen();
+}
+
