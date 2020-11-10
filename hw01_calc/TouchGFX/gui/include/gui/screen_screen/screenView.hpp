d30d26d13a7d48682f6858c6fe0e1623f1c5d4ee
Quence Z <qqq89513@yahoo.com.tw>
2020-11-11 03:27:38 +0800

    feat: Finish some buttons call back declartion

--- hw01_calc/TouchGFX/gui/include/gui/screen_screen/screenView.hpp
+++ hw01_calc/TouchGFX/gui/include/gui/screen_screen/screenView.hpp
@@ -11,7 +11,26 @@ public:
     virtual ~screenView() {}
     virtual void setupScreen();
     virtual void tearDownScreen();
+
 protected:
+    virtual void btn_0_onclick();
+    virtual void btn_1_onclick();
+    virtual void btn_2_onclick();
+    virtual void btn_3_onclick();
+    virtual void btn_4_onclick();
+    virtual void btn_5_onclick();
+    virtual void btn_6_onclick();
+    virtual void btn_7_onclick();
+    virtual void btn_8_onclick();
+    virtual void btn_9_onclick();
+    virtual void btn_00_onclick();
+    virtual void btn_dot_onclick();
+    virtual void btn_plus_onclick();
+    virtual void btn_minus_onclick();
+    virtual void btn_multiply_onclick();
+    virtual void btn_devide_onclick();
+    virtual void btn_equal_onclick();
+    virtual void btn_clear_onclick();
 };
 
 #endif // SCREENVIEW_HPP
--- hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
+++ hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
@@ -1,5 +1,4 @@
 #include <gui/screen_screen/screenView.hpp>
-#include <main.h>
 
 screenView::screenView()
 {
@@ -16,3 +15,24 @@ void screenView::tearDownScreen()
     screenViewBase::tearDownScreen();
 }
 
+void screenView::btn_0_onclick(){
+  static int clicks=0;
+  clicks++;
+};
+void screenView::btn_1_onclick(){};
+void screenView::btn_2_onclick(){};
+void screenView::btn_3_onclick(){};
+void screenView::btn_4_onclick(){};
+void screenView::btn_5_onclick(){};
+void screenView::btn_6_onclick(){};
+void screenView::btn_7_onclick(){};
+void screenView::btn_8_onclick(){};
+void screenView::btn_9_onclick(){};
+void screenView::btn_00_onclick(){};
+void screenView::btn_dot_onclick(){};
+void screenView::btn_plus_onclick(){};
+void screenView::btn_minus_onclick(){};
+void screenView::btn_multiply_onclick(){};
+void screenView::btn_devide_onclick(){};
+void screenView::btn_equal_onclick(){};
+void screenView::btn_clear_onclick(){};
