7c36f8df005aeef976e4f9f8e48453f170cb794d
Quence Z <qqq89513@yahoo.com.tw>
2020-11-12 21:30:46 +0800

    Feat: Display which button is clicked, continued 5c9a448
    Display on txt_debug, which is at left-bottom corner.

--- hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
+++ hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
@@ -1,5 +1,8 @@
 #include <gui/screen_screen/screenView.hpp>
 
+// show TypedText on specific text widget
+void showText(touchgfx::TextArea &txtWidget, TypedText txt);
+
 screenView::screenView()
 {
 
@@ -16,23 +19,64 @@ void screenView::tearDownScreen()
 }
 
 void screenView::btn_0_onclick(){
-  static int clicks=0;
-  clicks++;
-};
-void screenView::btn_1_onclick(){};
-void screenView::btn_2_onclick(){};
-void screenView::btn_3_onclick(){};
-void screenView::btn_4_onclick(){};
-void screenView::btn_5_onclick(){};
-void screenView::btn_6_onclick(){};
-void screenView::btn_7_onclick(){};
-void screenView::btn_8_onclick(){};
-void screenView::btn_9_onclick(){};
-void screenView::btn_00_onclick(){};
-void screenView::btn_dot_onclick(){};
-void screenView::btn_plus_onclick(){};
-void screenView::btn_minus_onclick(){};
-void screenView::btn_multiply_onclick(){};
-void screenView::btn_devide_onclick(){};
-void screenView::btn_equal_onclick(){};
-void screenView::btn_clear_onclick(){};
+  showText(txt_debug, btn_txt_0.getTypedText());
+}
+void screenView::btn_1_onclick(){
+  showText(txt_debug, btn_txt_1.getTypedText());
+}
+void screenView::btn_2_onclick(){
+  showText(txt_debug, btn_txt_2.getTypedText());
+}
+void screenView::btn_3_onclick(){
+  showText(txt_debug, btn_txt_3.getTypedText());
+}
+void screenView::btn_4_onclick(){
+  showText(txt_debug, btn_txt_4.getTypedText());
+}
+void screenView::btn_5_onclick(){
+  showText(txt_debug, btn_txt_5.getTypedText());
+}
+void screenView::btn_6_onclick(){
+  showText(txt_debug, btn_txt_6.getTypedText());
+}
+void screenView::btn_7_onclick(){
+  showText(txt_debug, btn_txt_7.getTypedText());
+}
+void screenView::btn_8_onclick(){
+  showText(txt_debug, btn_txt_8.getTypedText());
+}
+void screenView::btn_9_onclick(){
+  showText(txt_debug, btn_txt_9.getTypedText());
+}
+void screenView::btn_00_onclick(){
+  showText(txt_debug, btn_txt_00.getTypedText());
+}
+void screenView::btn_dot_onclick(){
+  showText(txt_debug, btn_txt_dot.getTypedText());
+}
+void screenView::btn_plus_onclick(){
+  showText(txt_debug, btn_txt_plus.getTypedText());
+}
+void screenView::btn_minus_onclick(){
+  showText(txt_debug, btn_txt_minus.getTypedText());
+}
+void screenView::btn_multiply_onclick(){
+  showText(txt_debug, btn_txt_multiply.getTypedText());
+}
+void screenView::btn_devide_onclick(){
+  showText(txt_debug, btn_txt_devide.getTypedText());
+}
+void screenView::btn_equal_onclick(){
+  showText(txt_debug, btn_txt_equal.getTypedText());
+}
+void screenView::btn_clear_onclick(){
+  showText(txt_debug, btn_txt_clear.getTypedText());
+}
+
+// show TypedText on specific text widget
+void showText(touchgfx::TextArea &txtWidget, TypedText txt){
+  txtWidget.setTypedText(txt);
+  txtWidget.invalidate();
+  txtWidget.resizeToCurrentText();
+  txtWidget.invalidate();
+}
