19f481bcebfe38dd627e61ab4755340a62952b39
Quence Z <qqq89513@yahoo.com.tw>
2020-11-13 15:06:24 +0800

    Refactor: Use wildcard of textArea to change the text
    The changes of machine generated codes are on last commit (3749d96).
    
    Before: Use TypedText to change the text. TypedText contains text, font, size, ...etc.
    Pros of this refactoring: Setting wildcard only requires Unicode string, which is more programmer friendly.
    See more about touchGFX textAreaWithOneWildcard: https://support.touchgfx.com/docs/api/classes/classtouchgfx_1_1_text_area_with_one_wildcard/

--- hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
+++ hw01_calc/TouchGFX/gui/src/screen_screen/screenView.cpp
@@ -1,7 +1,11 @@
 #include <gui/screen_screen/screenView.hpp>
 
-// show TypedText on specific text widget
-void showText(touchgfx::TextArea &txtWidget, TypedText txt);
+static const uint16_t TEXTAREA_SIZE = 16;
+touchgfx::Unicode::UnicodeChar textAreaBuffer[TEXTAREA_SIZE];
+
+// Show Unicode string or c-like string on TextArea that has one wildcard
+void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, const touchgfx::Unicode::UnicodeChar *str);
+void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, const char *str);  // c-like string
 
 screenView::screenView()
 {
@@ -19,64 +23,68 @@ void screenView::tearDownScreen()
 }
 
 void screenView::btn_0_onclick(){
-  showText(txt_debug, btn_txt_0.getTypedText());
+  showString(txt_debug, btn_txt_0.getTypedText().getText());
 }
 void screenView::btn_1_onclick(){
-  showText(txt_debug, btn_txt_1.getTypedText());
+  showString(txt_debug, btn_txt_1.getTypedText().getText());
 }
 void screenView::btn_2_onclick(){
-  showText(txt_debug, btn_txt_2.getTypedText());
+  showString(txt_debug, btn_txt_2.getTypedText().getText());
 }
 void screenView::btn_3_onclick(){
-  showText(txt_debug, btn_txt_3.getTypedText());
+  showString(txt_debug, btn_txt_3.getTypedText().getText());
 }
 void screenView::btn_4_onclick(){
-  showText(txt_debug, btn_txt_4.getTypedText());
+  showString(txt_debug, btn_txt_4.getTypedText().getText());
 }
 void screenView::btn_5_onclick(){
-  showText(txt_debug, btn_txt_5.getTypedText());
+  showString(txt_debug, btn_txt_5.getTypedText().getText());
 }
 void screenView::btn_6_onclick(){
-  showText(txt_debug, btn_txt_6.getTypedText());
+  showString(txt_debug, btn_txt_6.getTypedText().getText());
 }
 void screenView::btn_7_onclick(){
-  showText(txt_debug, btn_txt_7.getTypedText());
+  showString(txt_debug, btn_txt_7.getTypedText().getText());
 }
 void screenView::btn_8_onclick(){
-  showText(txt_debug, btn_txt_8.getTypedText());
+  showString(txt_debug, btn_txt_8.getTypedText().getText());
 }
 void screenView::btn_9_onclick(){
-  showText(txt_debug, btn_txt_9.getTypedText());
+  showString(txt_debug, btn_txt_9.getTypedText().getText());
 }
 void screenView::btn_00_onclick(){
-  showText(txt_debug, btn_txt_00.getTypedText());
+  showString(txt_debug, btn_txt_00.getTypedText().getText());
 }
 void screenView::btn_dot_onclick(){
-  showText(txt_debug, btn_txt_dot.getTypedText());
+  showString(txt_debug, btn_txt_dot.getTypedText().getText());
 }
 void screenView::btn_plus_onclick(){
-  showText(txt_debug, btn_txt_plus.getTypedText());
+  showString(txt_debug, btn_txt_plus.getTypedText().getText());
 }
 void screenView::btn_minus_onclick(){
-  showText(txt_debug, btn_txt_minus.getTypedText());
+  showString(txt_debug, btn_txt_minus.getTypedText().getText());
 }
 void screenView::btn_multiply_onclick(){
-  showText(txt_debug, btn_txt_multiply.getTypedText());
+  showString(txt_debug, btn_txt_multiply.getTypedText().getText());
 }
 void screenView::btn_devide_onclick(){
-  showText(txt_debug, btn_txt_devide.getTypedText());
+  showString(txt_debug, btn_txt_devide.getTypedText().getText());
 }
 void screenView::btn_equal_onclick(){
-  showText(txt_debug, btn_txt_equal.getTypedText());
+  showString(txt_debug, btn_txt_equal.getTypedText().getText());
 }
 void screenView::btn_clear_onclick(){
-  showText(txt_debug, btn_txt_clear.getTypedText());
+  showString(txt_debug, btn_txt_clear.getTypedText().getText());
+  showString(txt_result, "");
 }
 
-// show TypedText on specific text widget
-void showText(touchgfx::TextArea &txtWidget, TypedText txt){
-  txtWidget.setTypedText(txt);
+void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, const touchgfx::Unicode::UnicodeChar *str){
+  txtWidget.setWildcard(str);
   txtWidget.invalidate();
-  txtWidget.resizeToCurrentText();
+}
+
+void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, const char *str){
+  Unicode::snprintf(textAreaBuffer, TEXTAREA_SIZE, str);
+  txtWidget.setWildcard(textAreaBuffer);
   txtWidget.invalidate();
 }
