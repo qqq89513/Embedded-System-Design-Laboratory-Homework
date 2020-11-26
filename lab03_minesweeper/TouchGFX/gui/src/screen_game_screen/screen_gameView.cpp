736b1cd17c669420bd0e9fe3c3aa8212a85fbe26
Quence Z <qqq89513@yahoo.com.tw>
2020-11-26 11:50:51 +0800

    Feat: Forward the bomb counts to sreen_game

--- lab03_minesweeper/TouchGFX/gui/src/screen_game_screen/screen_gameView.cpp
+++ lab03_minesweeper/TouchGFX/gui/src/screen_game_screen/screen_gameView.cpp
@@ -1,5 +1,16 @@
 #include <gui/screen_game_screen/screen_gameView.hpp>
 
+// Globals
+// Show Unicode string or c-like string on TextArea that has one wildcard
+extern void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, const touchgfx::Unicode::UnicodeChar *str);
+extern void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, touchgfx::Unicode::UnicodeChar *buffer, const char *str); // show c-like string
+
+// String buffer for converting
+extern touchgfx::Unicode::UnicodeChar buffer_bomb_cnt[];
+extern touchgfx::Unicode::UnicodeChar buffer_debug[];
+extern char str_bomb[];  // c-like string that will be converted to oprd1 or oprd2
+extern int bomb_cnt;
+
 screen_gameView::screen_gameView()
 {
 
@@ -8,6 +19,7 @@ screen_gameView::screen_gameView()
 void screen_gameView::setupScreen()
 {
     screen_gameViewBase::setupScreen();
+    showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 
 void screen_gameView::tearDownScreen()
--- lab03_minesweeper/TouchGFX/gui/src/screen_screen/screenView.cpp
+++ lab03_minesweeper/TouchGFX/gui/src/screen_screen/screenView.cpp
@@ -1,13 +1,18 @@
 #include <gui/screen_screen/screenView.hpp>
 
 #include <stdio.h>      // using sprintf() for c-like string
-#include <string.h>
+#include <string.h>     // using strlen()
+#include <stdlib.h>     // using atoi()
+
+// Globals
+int bomb_cnt = 0;
+
 
 // String buffer for converting
-static const uint16_t TEXTAREA_SIZE = 32;
-touchgfx::Unicode::UnicodeChar buffer_result[TEXTAREA_SIZE];
+const uint16_t TEXTAREA_SIZE = 16;
+touchgfx::Unicode::UnicodeChar buffer_bomb_cnt[TEXTAREA_SIZE];
 touchgfx::Unicode::UnicodeChar buffer_debug[TEXTAREA_SIZE];
-static char str_bomb[TEXTAREA_SIZE];  // c-like string that will be converted to oprd1 or oprd2
+char str_bomb[TEXTAREA_SIZE];  // c-like string that will be converted to oprd1 or oprd2
 
 // Show Unicode string or c-like string on TextArea that has one wildcard
 void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, const touchgfx::Unicode::UnicodeChar *str);
@@ -26,60 +31,60 @@ void screenView::tearDownScreen(){
 void screenView::btn_0_onclick(){
   showString(txt_debug, txt_0.getTypedText().getText());
   addCharToStr(str_bomb, '0');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_1_onclick(){
   showString(txt_debug, txt_1.getTypedText().getText());
   addCharToStr(str_bomb, '1');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_2_onclick(){
   showString(txt_debug, txt_2.getTypedText().getText());
   addCharToStr(str_bomb, '2');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_3_onclick(){
   showString(txt_debug, txt_3.getTypedText().getText());
   addCharToStr(str_bomb, '3');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_4_onclick(){
   showString(txt_debug, txt_4.getTypedText().getText());
   addCharToStr(str_bomb, '4');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_5_onclick(){
   showString(txt_debug, txt_5.getTypedText().getText());
   addCharToStr(str_bomb, '5');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_6_onclick(){
   showString(txt_debug, txt_6.getTypedText().getText());
   addCharToStr(str_bomb, '6');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_7_onclick(){
   showString(txt_debug, txt_7.getTypedText().getText());
   addCharToStr(str_bomb, '7');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_8_onclick(){
   showString(txt_debug, txt_8.getTypedText().getText());
   addCharToStr(str_bomb, '8');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_9_onclick(){
   showString(txt_debug, txt_9.getTypedText().getText());
   addCharToStr(str_bomb, '9');
-  showString(txt_bomb_cnt, buffer_result, str_bomb);
+  showString(txt_bomb_cnt, buffer_bomb_cnt, str_bomb);
 }
 void screenView::btn_engage_onclick(){
-
+  bomb_cnt = atoi(str_bomb);
 }
 void screenView::btn_clear_onclick(){
   showString(txt_debug, txt_c.getTypedText().getText());
   str_bomb[0] = '\0';
-  showString(txt_bomb_cnt, buffer_result, "0");
+  showString(txt_bomb_cnt, buffer_bomb_cnt, "0");
 }
 
 void showString(touchgfx::TextAreaWithOneWildcard &txtWidget, const touchgfx::Unicode::UnicodeChar *str){
